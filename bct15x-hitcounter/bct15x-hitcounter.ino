e90530da61a4e174da27b4867490488c2ccedf42
erikorange <erikorange@gmail.com>
2023-02-28 21:20:57 -0500

    added working serial.txt code

--- bct15x-hitcounter/bct15x-hitcounter.ino
+++ bct15x-hitcounter/bct15x-hitcounter.ino
@@ -1,77 +1,52 @@
-#include <LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
 
-// LCD rows
-const int LCDROW1 = 0;
-const int LCDROW2 = 1;
-const int LCDROW3 = 2;
-const int LCDROW4 = 3;
+LiquidCrystal_I2C lcd(0x27, 16, 4);
 
-// I/O pins
-const int BUTTON_DOWN = 6;
-const int BUTTON_UP = 7;
-const int BUTTON_MAN = 8;
-const int BUTTON_SCAN = 9;
-const int BACKLIGHT = 13;
+#define LCD_ROW1 0
+#define LCD_ROW2 1
+#define LCD_ROW3 2
+#define LCD_ROW4 3
 
-// Definitions for button HIGH/LOW based on using pullup resistors
-const int BUTTON_OPEN = HIGH;
-const int BUTTON_CLOSED = LOW;
+#define bufSize 100
+char buffer[bufSize];
+bool gotResponse;
 
-// Connections:
-// rs (LCD pin 4) to Arduino pin 12
-// rw (LCD pin 5) to Arduino pin 11
-// enable (LCD pin 6) to Arduino pin 10
-// LCD pin 15 to Arduino pin 13
-// LCD pins d4, d5, d6, d7 to Arduino pins 5, 4, 3, 2
-LiquidCrystal lcd(12, 11, 10, 5, 4, 3, 2);
-
-
-
-
-
-
-
-
-
-int bufSize = 100;
-
-char buffer[100];
-
-void setup() {
-  // Configure input pushbuttons
-  pinMode(BUTTON_DOWN, INPUT);
-  digitalWrite(BUTTON_DOWN, HIGH);     // turn on the built in pull-up resistor
-  
-  pinMode(BUTTON_UP, INPUT);
-  digitalWrite(BUTTON_UP, HIGH);       // turn on the built in pull-up resistor
-  
-  pinMode(BUTTON_MAN, INPUT);
-  digitalWrite(BUTTON_MAN, HIGH);      // turn on the built in pull-up resistor
-  
-  pinMode(BUTTON_SCAN, INPUT);
-  digitalWrite(BUTTON_SCAN, HIGH);     // turn on the built in pull-up resistor
-
-  // Configure backlight
-  pinMode(BACKLIGHT, OUTPUT);
-  digitalWrite(BACKLIGHT, HIGH);
-
-  lcd.begin(20, 4);
-  lcd.clear();
-
+void setup()
+{
+  lcd.init();
+  lcd.backlight();
   Serial.begin(19200);
-
   DisplayTitle();
 }
 
-void loop() {
-  //Serial.write("GLG\r");
-  delay(100);
-  //recvWithEndMarker();
-  delay(100);
+void loop()
+{
+  Serial.print("GLG\r");
+
+  while (!recvWithEndMarker())
+  {
+    lcd.setCursor(1, LCD_ROW1);
+    lcd.print("Waiting for data");
+    delay(200);
+    lcd.clear();
+  }
+
+  lcd.clear();
+  lcd.setCursor(1, LCD_ROW2);
+  lcd.print("Got data");
+
+  for (int i = 0; i<18; i++)
+  {
+    lcd.setCursor(i+1, LCD_ROW3);
+    lcd.print(buffer[i]);
+  }
+  delay(2000);
+    
+  lcd.clear();
+  delay(2000);
 }
 
-
-void recvWithEndMarker() {
+bool recvWithEndMarker() {
     bool done = false;
     int idx = 0;
     char endMarker = '\r';
@@ -79,7 +54,7 @@ void recvWithEndMarker() {
     
     if (Serial.available() == 0)
     {
-      return;
+      return false;
     }
 
     while (Serial.available() > 0 && done == false) {
@@ -97,17 +72,18 @@ void recvWithEndMarker() {
             done = true;
         }
     }
-    Serial.println(buffer);
+    return true;
 }
 
 // Display the opening title
 void DisplayTitle()
 {
-  lcd.setCursor(1, LCDROW1);
+  lcd.setCursor(1, LCD_ROW1);
   lcd.print("BCT15X Hit Counter");
-  lcd.setCursor(4, LCDROW2);
+  lcd.setCursor(4, LCD_ROW2);
   lcd.print("Version 1.0.0");
-  lcd.setCursor(2, LCDROW3);
+  lcd.setCursor(2, LCD_ROW3);
   lcd.print("(c) Erik Orange");
   delay(2000);
+  lcd.clear();
 }
