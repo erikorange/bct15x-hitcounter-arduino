6934b4b7e69a44c8c9dce6da7dabcbc6fb33a216
erikorange <erikorange@gmail.com>
2025-03-22 21:22:17 -0400

    added hit counter - single display

--- bct15x-hitcounter/bct15x-hitcounter.ino
+++ bct15x-hitcounter/bct15x-hitcounter.ino
@@ -1,6 +1,6 @@
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 20, 4);
+LiquidCrystal_I2C lcd_1(0x27, 20, 4);
 
 #define LCD_ROW1 0
 #define LCD_ROW2 1
@@ -12,18 +12,33 @@ LiquidCrystal_I2C lcd(0x27, 20, 4);
 #define TAG_LEN 17
 #define SYSNAME_LEN 17
 
+#define HIT_ARRAY_SIZE 25
+#define HIT_ARRAY_FULL -1
+#define HIT_NOT_FOUND -1
+
 #define ACTIVITY_DOTS_X 15
 #define NUM_ACTIVITY_DOTS 8
 
 char buffer[BUFSIZE];
 
+typedef struct {
+    int count;
+    char freq[FREQ_LEN];
+    char alphaTag[TAG_LEN];
+} Hit;
+
+Hit hits[HIT_ARRAY_SIZE];
+
+
 char freq[FREQ_LEN];
 char alphaTag[TAG_LEN];
 char sysName[SYSNAME_LEN];
 bool gotHit;
+bool foundAHit;
 int spinIdx;
-long mark;
-
+long serialMark, hitMark;
+int hitDisplayIdx;
+bool foundNextDisplayHit;
 
 char *activityDots[] = {
   ".    \0",
@@ -38,16 +53,24 @@ char *activityDots[] = {
 
 void setup()
 {
-  lcd.init();
-  lcd.backlight();
-  lcd.clear();
+  lcd_1.init();
+  lcd_1.backlight();
+  lcd_1.clear();
   Serial.begin(19200);
   DisplayTitle();
+
+  initializeHits();
+
   gotHit = false;
-  mark = millis();
-  lcd.setCursor(ACTIVITY_DOTS_X, LCD_ROW4);
+  foundAHit = false;
+  serialMark = millis();
+  hitMark = millis();
+
+  hitDisplayIdx = 0;
+  
+  lcd_1.setCursor(ACTIVITY_DOTS_X, LCD_ROW4);
   spinIdx = 0;
-  lcd.print(activityDots[spinIdx]);
+  lcd_1.print(activityDots[spinIdx]);
 }
 
 void loop()
@@ -62,20 +85,26 @@ void loop()
     {
       if (!gotHit && isSquelchOpen(buffer))
       {
+        foundAHit = true;
         gotHit = true;
-        lcd.clear();
+        
+        /* lcd_1.clear(); */
 
         getFreq(buffer, freq);
-        lcd.setCursor(0,0);
-        lcd.print(freq);
+        /*lcd_1.setCursor(0,LCD_ROW1);
+        lcd_1.print(freq);
+        lcd_1.setCursor(8, LCD_ROW1);
+        lcd_1.print("MHz"); */
 
         getAlphaTag(buffer, alphaTag);
-        lcd.setCursor(0,1);
-        lcd.print(alphaTag);
+        /* lcd_1.setCursor(0,LCD_ROW2);
+        lcd_1.print(alphaTag); */
 
-        getSysName(buffer, sysName);
-        lcd.setCursor(0,2);
-        lcd.print(sysName);                       
+        /*getSysName(buffer, sysName);
+        lcd_1.setCursor(0,LCD_ROW3);
+        lcd_1.print(sysName);
+        */
+        addToHitListfreq(freq, alphaTag);                 
       }
 
       if (gotHit && !isSquelchOpen(buffer))
@@ -85,9 +114,10 @@ void loop()
     }
   }
 
-  if (millis() - mark > 250)
+/*
+  if (millis() - serialMark > 250)
   {
-    mark = millis();
+    serialMark = millis();
 
     spinIdx++;
     if (spinIdx == NUM_ACTIVITY_DOTS)
@@ -95,9 +125,53 @@ void loop()
       spinIdx = 0;
     }
     
-    lcd.setCursor(ACTIVITY_DOTS_X, LCD_ROW4);
-    lcd.print(activityDots[spinIdx]);
+    lcd_1.setCursor(ACTIVITY_DOTS_X, LCD_ROW4);
+    lcd_1.print(activityDots[spinIdx]);
   }
+*/
+
+  if (millis() - hitMark > 2000)
+  {
+    if (foundAHit)
+    {
+      foundNextDisplayHit = false;
+      do
+      {
+        if (hits[hitDisplayIdx].count != 0)
+        {
+          foundNextDisplayHit = true;
+
+          /* display the hit */
+          lcd_1.clear();
+
+          lcd_1.setCursor(0,LCD_ROW1);
+          lcd_1.print(hits[hitDisplayIdx].freq);
+          lcd_1.setCursor(8, LCD_ROW1);
+          lcd_1.print("MHz");
+
+          lcd_1.setCursor(0,LCD_ROW2);
+          lcd_1.print(hits[hitDisplayIdx].alphaTag);
+          lcd_1.setCursor(0,LCD_ROW3);
+          lcd_1.print("Hits: ");
+          lcd_1.setCursor(6, LCD_ROW3);
+          lcd_1.print(hits[hitDisplayIdx].count);
+
+          hitDisplayIdx++;
+        }
+        else
+        {
+          hitDisplayIdx++;
+          if (hitDisplayIdx == HIT_ARRAY_SIZE)
+          {
+            hitDisplayIdx = 0;
+          }
+        }
+      }
+      while (!foundNextDisplayHit);
+    }
+    hitMark = millis();
+  }
+
 }
 
 void clearBuffer(char* buf)
@@ -194,14 +268,79 @@ bool isSquelchOpen(char* buffer)
   return false;
 }
 
+void initializeHits()
+{
+  for (int i = 0; i < HIT_ARRAY_SIZE; i++)
+  {
+    hits[i].count = 0;
+
+    for (int j = 0; j < FREQ_LEN; j++)
+    {
+      hits[i].freq[j] = '\0';
+    }
+
+    for (int j = 0; j < TAG_LEN; j++)
+    {
+      hits[i].alphaTag[j] = '\0';
+    }
+  }
+}
+
+void addToHitListfreq(char* freq, char* alphaTag)
+{
+  int i = findHit(freq, alphaTag);
+  if (i == HIT_NOT_FOUND)
+  {
+    int j = findNextHitSlot();
+    if (j == HIT_ARRAY_FULL)
+    {
+      return;
+    }
+    else
+    {
+      strcpy(hits[j].freq, freq);
+      strcpy(hits[j].alphaTag, alphaTag);
+      hits[j].count++;
+    }
+  }
+  else
+  {
+    hits[i].count++;
+  }
+}
+
+int findHit(char* freq, char* alphaTag)
+{
+  for (int i = 0; i < HIT_ARRAY_SIZE; i++)
+  {
+    if ( (strcmp(freq, hits[i].freq) == 0) && (strcmp(alphaTag, hits[i].alphaTag) == 0) )
+    return i;
+  }
+
+  return HIT_NOT_FOUND;
+}
+
+int findNextHitSlot()
+{
+  for (int i = 0; i < HIT_ARRAY_SIZE; i++)
+  {
+    if (hits[i].count == 0)
+    {
+      return i;
+    }
+  }
+  return HIT_ARRAY_FULL;
+}
+
+
 void DisplayTitle()
 {
-  lcd.setCursor(3, LCD_ROW1);
-  lcd.print("BCT15X Display");
-  lcd.setCursor(4, LCD_ROW2);
-  lcd.print("Version 1.1.0");
-  lcd.setCursor(2, LCD_ROW3);
-  lcd.print("(c) Erik Orange");
+  lcd_1.setCursor(3, LCD_ROW1);
+  lcd_1.print("BCT15X Display");
+  lcd_1.setCursor(4, LCD_ROW2);
+  lcd_1.print("Version 1.1.0");
+  lcd_1.setCursor(2, LCD_ROW3);
+  lcd_1.print("(c) Erik Orange");
   delay(1000);
-  lcd.clear();
+  lcd_1.clear();
 }
